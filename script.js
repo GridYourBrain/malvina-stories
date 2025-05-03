c5e5c1ad6dd2a87ca3ee31c9af0f8ed2ec6f464d
GridYourBrain <gridyourbrain@gmail.com>
2025-05-03 23:42:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 document.getElementById('storyForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    const name = this.name.value;
-    const theme = this.theme.value;
-    const character = this.character.value;
-
-    const story = `${name} тръгна на вълшебно приключение с ${character}. Тяхната тема беше: ${theme}. И всичко завърши щастливо!`;
-
-    document.getElementById('storyOutput').innerText = story;
+    const theme = document.getElementById('theme').value;
+    const character = document.getElementById('character').value;
+    const emotion = document.getElementById('emotion').value;
+    document.getElementById('storyOutput').innerText = `Приказка за ${character}, който открива ${emotion} през темата за ${theme}.`;
 });
